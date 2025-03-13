4351704d1fc44429e6bc8269865139210db601d6
Maharjit_426 <maharjit.adhikary123@gmail.com>
2025-03-14 00:30:22 +0530

    Create WB_MUX.v

---
+++ WB_MUX.v
@@ -0,0 +1,13 @@
+module four_mux(
+  input reg[31:0]in1,in2,in3,
+  input [1:0]sel,
+  output reg[31:0]out);
+  always@(*) begin
+    case(sel)
+      2'b00 : out = in1;
+      2'b01 : out = in2;
+      2'b10 : out = in3;
+      default : out = in1;
+    endcase
+  end
+endmodule
